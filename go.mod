8c3a14fe783bcc69acb10014af3b451a03d35360
samsam <226710873+samsam-0412@users.noreply.github.com>
2026-04-26 10:10:37 +0200

    ui looks better now need to move the whole code into their on files

--- go.mod
+++ go.mod
@@ -6,13 +6,11 @@ require (
 	github.com/JoshGuarino/PokeGo v1.2.3
 	github.com/alan-ar1/imgfetch v0.0.0-20260205152321-ed46e26b1761
 	github.com/gizak/termui/v3 v3.1.0
-	github.com/kevin-cantwell/dotmatrix v0.2.3
+	golang.org/x/crypto v0.50.0
 	gopkg.in/gographics/imagick.v3 v3.7.3
 )
 
 require (
-	github.com/asticode/go-astiav v0.40.0 // indirect
-	github.com/asticode/go-astikit v0.42.0 // indirect
 	github.com/aymanbagabas/go-osc52/v2 v2.0.1 // indirect
 	github.com/charmbracelet/colorprofile v0.2.3-0.20250311203215-f60798e515dc // indirect
 	github.com/charmbracelet/lipgloss v1.1.0 // indirect
@@ -30,5 +28,6 @@ require (
 	github.com/rivo/uniseg v0.4.7 // indirect
 	github.com/xo/terminfo v0.0.0-20220910002029-abceb7e1c41e // indirect
 	golang.org/x/exp v0.0.0-20231006140011-7918f672742d // indirect
-	golang.org/x/sys v0.37.0 // indirect
+	golang.org/x/sys v0.43.0 // indirect
+	golang.org/x/term v0.42.0 // indirect
 )
--- internal/tui/tui.go
+++ internal/tui/tui.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"bufio"
 	"encoding/json"
 	"fmt"
 	"image"
@@ -9,7 +8,7 @@ import (
 	_ "image/jpeg"
 	_ "image/png"
 	"log"
-	"math/rand/v2"
+	"math/rand"
 	"os"
 	spriteDownload "pokeinfotui/internal/download"
 	"pokeinfotui/internal/handler"
@@ -21,33 +20,25 @@ import (
 	"github.com/JoshGuarino/PokeGo/pkg/models"
 	ui "github.com/gizak/termui/v3"
 	"github.com/gizak/termui/v3/widgets"
+	//terminal "golang.org/x/crypto/ssh/terminal"
 )
 
 func main() {
-	args := os.Args
-	var random int
+	var input string
 	var err error
 	var s *models.Pokemon
 	client := pokego.NewClient()
-	if args[0] == "random" {
-		random = rand.IntN(1025-1) + 1
-		s, err = client.Pokemon.GetPokemon(strconv.Itoa(random))
-		if err != nil {
-			log.Fatal(err)
-		}
-	} else {
-		fmt.Print("Enter a Pokemon name or pokedexId: ")
-		scanner := bufio.NewScanner(os.Stdin)
-		scanner.Scan()
-		err := scanner.Err()
-		if err != nil {
-			log.Fatal(err)
-		}
-		s, err = client.Pokemon.GetPokemon(scanner.Text())
-		if err != nil {
-			log.Fatal(err)
-		}
+	fmt.Print("Enter a Pokemon name or pokedexId or random: ")
+	fmt.Scanln(&input)
+	if input == "random" {
+		input = strconv.Itoa(rand.Intn(1025-1) + 1)
 	}
+	s, err = client.Pokemon.GetPokemon(input)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	//ttyWidth, ttyHheight, err := terminal.GetSize(0)
 	url := s.Sprites.FrontDefault
 	filename := s.Name + ".png"
 	spriteDownload.DownloadPrep(url, filename)
@@ -57,15 +48,12 @@ func main() {
 	var stringTypes string
 	TypesList := s.Types
 	for index := range TypesList {
-
 		re := regexp.MustCompile(`[^\d\p{Latin}]`)
-
 		currentElement, err := json.Marshal(TypesList[index].Type.Name)
 		if err != nil {
 		}
 		jsonStr := re.ReplaceAllString(string(currentElement), "")
 		stringTypes += jsonStr + "\n"
-
 	}
 	var stringAbilityList [100]string
 	abilityList := s.Abilities
@@ -108,61 +96,61 @@ func main() {
 	if err != nil {
 		log.Println("Cannot decode file:", err)
 	}
-	img := widgets.NewImage(nil)
+	img := widgets.NewImage(image)
 	img.SetRect(0, 0, 40, 40)
 
 	name := widgets.NewParagraph()
 	name.Title = "Name"
 	name.Text = s.Name
-	name.SetRect(60, 0, 95, 3)
+	name.SetRect(40, 0, 75, 3)
 
 	height := widgets.NewParagraph()
 	height.Title = "Height"
 	height.Text = strconv.Itoa(s.Height) + " m"
-	height.SetRect(60, 3, 95, 6)
+	height.SetRect(40, 3, 75, 6)
 
 	weight := widgets.NewParagraph()
 	weight.Title = "Weight"
 	weight.Text = strconv.Itoa(s.Weight) + " kg"
-	weight.SetRect(60, 6, 95, 9)
+	weight.SetRect(40, 6, 75, 9)
 
 	abilityListWidget := widgets.NewList()
 	abilityListWidget.Title = "Abilities"
 	abilityListWidget.Rows = stringAbilityList[:]
 	abilityListWidget.TextStyle = ui.NewStyle(ui.ColorYellow)
 	abilityListWidget.WrapText = false
-	abilityListWidget.SetRect(80, 9, 115, 15)
+	abilityListWidget.SetRect(40, 9, 75, 15)
 
 	moveListWidget := widgets.NewList()
 	moveListWidget.Title = "Moves"
 	moveListWidget.Rows = stringMoveList[:]
 	moveListWidget.TextStyle = ui.NewStyle(ui.ColorYellow)
 	moveListWidget.WrapText = false
-	moveListWidget.SetRect(60, 15, 95, 21)
+	moveListWidget.SetRect(40, 15, 75, 21)
 
 	typesWidget := widgets.NewParagraph()
 	typesWidget.Title = "Types"
 	typesWidget.Text = stringTypes
-	typesWidget.SetRect(60, 21, 95, 25)
+	typesWidget.SetRect(40, 21, 75, 25)
 
 	keybind := widgets.NewParagraph()
 	keybind.Title = "Keybinds"
-	keybind.SetRect(60, 29, 116, 40)
+	keybind.SetRect(40, 29, 76, 40)
 
 	scrollAbilities := widgets.NewParagraph()
 	scrollAbilities.Title = "Abilities"
 	scrollAbilities.Text = "Scroll Abilities: K and J or Down and Up (Arrows)"
-	scrollAbilities.SetRect(61, 30, 115, 33)
+	scrollAbilities.SetRect(41, 30, 75, 33)
 
 	scrollMove := widgets.NewParagraph()
 	scrollMove.Title = "Abilities"
 	scrollMove.Text = "Scroll Moves: H and L or Left and Right (Arrows)"
-	scrollMove.SetRect(61, 33, 115, 36)
+	scrollMove.SetRect(41, 33, 75, 36)
 
 	quit := widgets.NewParagraph()
 	quit.Title = "Exit"
 	quit.Text = "Press q to quit"
-	quit.SetRect(61, 36, 115, 39)
+	quit.SetRect(41, 36, 75, 39)
 	render := func() {
 		img.Image = image
 		ui.Render(img, name, height, weight, abilityListWidget, moveListWidget, typesWidget, keybind, quit, scrollAbilities, scrollMove)
